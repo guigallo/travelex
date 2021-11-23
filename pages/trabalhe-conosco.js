8f5cb427fc6feaa94cf8c9d99c99d695639e1ac5
Guilherme <guilherme.gg1@hotmail.com>
2021-11-22 22:55:06 -0300

    feat(job): locales and footer

--- pages/trabalhe-conosco.js
+++ pages/trabalhe-conosco.js
@@ -1,21 +1,26 @@
 import { useTranslations } from 'use-intl'
 import FormPage from '@/components/FormPage'
 import { FormTypes } from '@/components/RegisterForm'
+import Footer from '@/components/Layout/Footer'
+import Scroller from '@/components/Scroller'
 import imgBannerTwo from '../public/images/bannerTwoHome.png'
 
 function Cadastro(props) {
   const translate = useTranslations('Jobs')
 
   return (
-    <FormPage
-      backgroundImage={imgBannerTwo}
-      description={translate('description')}
-      descriptionTitle={translate('descriptionTitle')}
-      formType={FormTypes.TRABALHE_CONOSCO}
-      hideFormType
-      theme="light"
-      {...props}
-    />
+    <Scroller>
+      <FormPage
+        backgroundImage={imgBannerTwo}
+        description={translate('description')}
+        descriptionTitle={translate('descriptionTitle')}
+        formType={FormTypes.TRABALHE_CONOSCO}
+        hideFormType
+        theme="light"
+        {...props}
+      />
+      <Footer />
+    </Scroller>
   )
 }
 
